f8caf7cdbb20debf847540736433229182367388
jerminiplap <jerministephane@gmail.com>
2020-01-07 14:15:39 +0100

    Modification fichier settings.py pour production

--- AmorWebsite/settings.py
+++ AmorWebsite/settings.py
@@ -103,22 +103,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'AmorWebsite.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    #'default': {
-    #    'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #}
+
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "amorDataBase",
-        "USER": "postgres",
-        "PASSWORD": "jnzmYPpMe34",
+        "NAME": "amorDB",
+        "USER": "webadmin",
+        "PASSWORD": "RSSncz24152",
         "HOST": "localhost",
-        "PORT": "5432",
+        "PORT": "",
     }
 }
 
@@ -161,8 +157,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
-
 
 STATIC_URL = '/static/'
 
